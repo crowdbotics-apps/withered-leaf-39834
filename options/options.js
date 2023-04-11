5be8c2e201572b56247194cfec93e1674e00f835
Crowdbotics <team@crowdbotics.com>
2023-04-11 14:10:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-leaf-39834",
+  url: "https://withered-leaf-39834.botics.co",
+  api: "https://withered-leaf-39834.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
